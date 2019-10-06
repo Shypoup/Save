c3ceb578fd9f489a0d0be6d2032b4785502ce0ed
yassertarek <yasser.tarek@uflare.io>
2019-10-06 21:11:50 +0200

    try_dirty_code

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {View,Button,StyleSheet,PermissionsAndroid,Linking,Text, AsyncStorage,YellowBox} from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
+import wifi from 'react-native-android-wifi';
+import Geolocation from 'react-native-geolocation-service';
+import Geocoder from 'react-native-geocoder';
 import DataAndWifi from './mobile_services/data_wifi';
 import FindLoction from './mobile_services/find_location';
 import SendMail from './mobile_services/send_mail';
@@ -12,85 +15,70 @@ export default class App extends React.Component{
     formattedAddress: ''
   };
 
-//   async requestLocationPermission() {
-//     const chckLocationPermission = PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
-//     if (chckLocationPermission === PermissionsAndroid.RESULTS.GRANTED) {
-//         console.log("You've access for the location");
-//     } else {
-//         try {
-//             const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-//                 {
-//                     'title': 'Cool Location App required Location permission',
-//                     'message': 'We required Location permission in order to get device location ' +
-//                         'Please grant us.'
-//                 }
-//             )
-//             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-//               // Geocoder.geocodeAddress('New Cairo').then(res => {
-//               //   this.addr = res[0].position;
-//               //   console.log(this.addr);
-//               //   const address = this.addr;
+  async requestLocationPermission() {
+    const chckLocationPermission = PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    if (chckLocationPermission === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You've access for the location");
+    } else {
+        try {
+            const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    'title': 'Cool Location App required Location permission',
+                    'message': 'We required Location permission in order to get device location ' +
+                        'Please grant us.'
+                }
+            )
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              // Geocoder.geocodeAddress('New Cairo').then(res => {
+              //   this.addr = res[0].position;
+              //   console.log(this.addr);
+              //   const address = this.addr;
                   
                   
-//               // }) .catch(err => console.log(err))
-//               Geolocation.getCurrentPosition(
-//                 (position) => {
-//                      console.log(position);
-//                      Geocoder.geocodePosition({lat:position.coords.latitude,lng:position.coords.longitude}).then((res) => {
-//                       // let data = res.filter(info => info.position.lng == position.coords.longitude && info.position.lat == position.coords.latitude);
-//                       //  console.log(data);
-//                       console.log(res);
-//                       return this.setState({formattedAddress: res[0].formattedAddress})
+              // }) .catch(err => console.log(err))
+              Geolocation.getCurrentPosition(
+                (position) => {
+                     console.log(position);
+                     Geocoder.geocodePosition({lat:position.coords.latitude,lng:position.coords.longitude}).then((res) => {
+                      // let data = res.filter(info => info.position.lng == position.coords.longitude && info.position.lat == position.coords.latitude);
+                      //  console.log(data);
+                      console.log(res);
+                      return this.setState({formattedAddress: res[0].formattedAddress})
                   
-//                   }).catch(err => console.log(err))
-//                 }, 
-//                 (error) => {
-//                     // See error code charts below.
-//                     console.log(error.code, error.message);
-//                 },
-//                 { enableHighAccuracy: false, timeout: 15000, maximumAge: 10000, showLocationDialog: false }
-//             );
-//             } else {
-//                 alert("You don't have access for the location");
-//             }
-//         } catch (err) {
-//             alert(err)
-//         }
-//     }
-// };
+                  }).catch(err => console.log(err))
+                }, 
+                (error) => {
+                    // See error code charts below.
+                    console.log(error.code, error.message);
+                },
+                { enableHighAccuracy: false, timeout: 15000, maximumAge: 10000, showLocationDialog: false }
+            );
+            } else {
+                alert("You don't have access for the location");
+            }
+        } catch (err) {
+            alert(err)
+        }
+    }
+};
 
 
   render(){
     return(
 
       <View style={styles.container}>
-          <Button
-          title="get_Address"
-          onPress={async ()=>{
-             const obj = new FindLoction();
-             await obj.Get_Location();
-             this.setState({
-               formattedAddress : await AsyncStorage.getItem('Address')
-              
-              })
-            }
-          }
-          ></Button>
+          
 
           <Button 
-           title="wifi"
-           onPress={()=>{new DataAndWifi().open_Phone_Wifi();}}
+           title="Danger_Mode"
+           onPress={()=>{
+             this.requestLocationPermission();
+           }}
           >
 
           </Button>
 
-          <Button
-            title = 'send_Mail'
-            onPress = {() => {new SendMail().Send_Email()}}
-          >
-
-          </Button>
-
+          
           <Text>{this.state.formattedAddress}</Text>
 
           <QRCode
--- mobile_services/data_wifi.js
+++ mobile_services/data_wifi.js
@@ -4,7 +4,13 @@ import wifi from 'react-native-android-wifi';
 export default class DataAndWifi extends React.Component{
 
      open_Phone_Wifi = ()=>{
-        wifi.setEnabled(true);
+        wifi.isEnabled((isEnabled) => {
+            if (isEnabled) {
+              console.log("wifi service enabled");
+            } else {
+              wifi.setEnabled(true);
+            }
+          });
      }
 
      open_Phone_Data = ()=>{
